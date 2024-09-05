623dea48ca194f8ee6aeac1254b2d8e896796ac6
Jacob Roberts <jroberts.g1ql8@simplelogin.com>
2024-09-05 15:48:00 +0100

    Fix the tweet page twitter ID

--- src/app/ppr/page.tsx
+++ src/app/ppr/page.tsx
@@ -19,11 +19,11 @@ const TweetPage = async ({ id }: { id: string }) => {
 	}
 };
 
-const Page = ({ params }: { params: { tweet: string } }) => (
+const Page = () => (
 	<div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
 		<main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
 			<Suspense fallback={<TweetSkeleton />}>
-				<TweetPage id={params.tweet} />
+				<TweetPage id="1801907412175044774" />
 			</Suspense>
 			<footer>Jake's static footer</footer>
 		</main>
