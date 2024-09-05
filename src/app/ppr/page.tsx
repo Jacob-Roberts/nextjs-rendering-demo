2a0379a1a5a4f1759c92c145d46b2b5161a2c44b
Jacob Roberts <jroberts.g1ql8@simplelogin.com>
2024-09-05 16:44:49 +0100

    Add some randomness to tweet page

--- src/app/ppr/page.tsx
+++ src/app/ppr/page.tsx
@@ -5,9 +5,16 @@ import { getTweet } from "react-tweet/api";
 
 export const experimental_ppr = true;
 
-const TweetPage = async ({ id }: { id: string }) => {
+const TweetPage = async () => {
 	const h = headers();
 	const backupId = h.get("test");
+
+	let id = "1798389854537126063";
+	if (Math.random() < 0.5) {
+		id = "1801907412175044774";
+	} else if (Math.random() < 0.1) {
+		id = "";
+	}
 	try {
 		const tweet = await getTweet(id ?? backupId);
 		return tweet ? <EmbeddedTweet tweet={tweet} /> : <TweetNotFound />;
@@ -21,7 +28,7 @@ const Page = () => (
 	<div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
 		<main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
 			<Suspense fallback={<TweetSkeleton />}>
-				<TweetPage id="1801907412175044774" />
+				<TweetPage />
 			</Suspense>
 			<footer>Jake's static footer</footer>
 		</main>
